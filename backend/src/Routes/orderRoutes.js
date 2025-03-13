b65808d4c9cc430db93b04456bc0f7abb53a520b
PRAVEEN KANNA K R <praveenkannakr@PRAVEENS-MAC.local>
2025-03-13 09:13:53 +0530

    Completed

--- backend/src/Routes/orderRoutes.js
+++ backend/src/Routes/orderRoutes.js
@@ -1,17 +1,25 @@
 const express = require("express");
 const router = express.Router();
-const Order = require("../Model/order.js");
+const Order = require("../models/order.js"); 
+
 
 router.post("/place-order", async (req, res) => {
   try {
     const { userId, address, products, totalAmount } = req.body;
+
+
+    if (!userId || !address || !products || products.length === 0 || !totalAmount) {
+      return res.status(400).json({ message: "All fields are required, including products" });
+    }
+
 
     const newOrder = new Order({ userId, address, products, totalAmount });
     await newOrder.save();
 
     res.status(201).json({ message: "Order placed successfully", order: newOrder });
   } catch (error) {
-    res.status(500).json({ message: "Server Error", error });
+    console.error("Error placing order:", error);
+    res.status(500).json({ message: "Server Error", error: error.message });
   }
 });
 
